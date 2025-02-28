98dff16ac370616964c5b4d5a2c9dee62ad6fbb5
platonic <minlu0954@gmail.com>
2025-02-28 19:34:13 +0630

    backend Finish

---
+++ MERN-CRUSH-COURSE/backend/routes/product.route.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+  CreateProduct,
+  deleteProduct,
+  getProducts,
+  updateProduct,
+} from "../controllers/product.controller.js";
+
+const router = express.Router();
+
+router.get("/", getProducts);
+
+router.post("/", CreateProduct);
+
+router.put("/:id", updateProduct);
+
+router.delete("/:id", deleteProduct);
+
+export default router;
