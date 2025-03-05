b4700b370e35b05f8f3f123a4974bb7a6734cdb5
platonic <minlu0954@gmail.com>
2025-03-05 22:51:32 +0630

    protuction complete

--- MERN-CRUSH-COURSE/backend/server.js
+++ MERN-CRUSH-COURSE/backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 
@@ -8,10 +9,21 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const __dirname = path.resolve();
 //middleWare
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  // Serve static files from the "frontend/dist" directory
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  // Handle all other routes by serving the "index.html" file
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
