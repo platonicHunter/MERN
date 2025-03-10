70dbb5e4c2bd9b2ae54b863dab6344829bf0bbdc
platonic <minlu0954@gmail.com>
2025-03-10 18:10:53 +0630

    1-Project Step finish

--- MERN-CRUSH-COURSE/backend/server.js
+++ MERN-CRUSH-COURSE/backend/server.js
@@ -7,7 +7,7 @@ import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 //middleWare
@@ -17,11 +17,11 @@ app.use("/api/products", productRoutes);
 
 if (process.env.NODE_ENV === "production") {
   // Serve static files from the "frontend/dist" directory
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   // Handle all other routes by serving the "index.html" file
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
   });
 }
 
